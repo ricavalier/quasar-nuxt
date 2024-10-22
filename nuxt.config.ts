d1615f9b606b3446e58c0acb5b6748732edf91c9
Ricardo Cavalier <ricavalier@hotmail.com>
2024-10-22 11:29:26 -0300

    feat(proj): Basic project Quasar Nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ['@/assets/custom.scss'],
+  modules: ['nuxt-quasar-ui'],
+
+  quasar: {
+    plugins: [
+      'BottomSheet',
+      'Dialog',
+      'Loading',
+      'LoadingBar',
+      'Notify',
+      'Dark',
+    ],
+    extras: {
+      font: 'roboto-font',
+    },
+    components: {
+      defaults: {
+        QBtn: {
+          unelevated: true,
+        },
+      },
+    },
+  },
+  compatibilityDate: '2024-08-22',
+});
